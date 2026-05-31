e96431b19787e9b71583f25685f5a1c948194e63
AustinOffc <achmuhlis77@gmail.com>
2026-05-31 11:27:13 +0700

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ fi
 
 clear
 echo "----------------------------------------------------------"
-echo "                       AUSTIN STORE                       "
+echo "               AUSTIN STORE OFFICIAL                      "
 echo "----------------------------------------------------------"
 echo "Location   : $(curl -s ipinfo.io/country 2>/dev/null || echo 'Unknown')"
 echo "OS         : $(grep -oP '(?<=^PRETTY_NAME=).+' /etc/os-release | tr -d '\"')"
